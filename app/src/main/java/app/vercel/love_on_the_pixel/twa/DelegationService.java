6bfa59cea22649938e15bebb9ae5c715abd3310b
CoryNation <cory.nation@buildtbd.com>
2025-08-25 16:16:09 -0400

    Add privacy policy page for Google Play Store compliance

---
+++ app/src/main/java/app/vercel/love_on_the_pixel/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.love_on_the_pixel.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
