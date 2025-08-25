c777b8ba6b633795dac3ef06f4a6e94bf2564b37
CoryNation <cory.nation@buildtbd.com>
2025-08-25 17:41:58 -0400

    fixing 404 issue

--- next.config.js
+++ next.config.js
@@ -15,16 +15,4 @@ const nextConfig = {
   }
 };
 
-// Add PWA configuration conditionally
-if (process.env.NODE_ENV === 'production') {
-  const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: false
-  });
-  
-  module.exports = withPWA(nextConfig);
-} else {
-  module.exports = nextConfig;
-}
+module.exports = nextConfig;
